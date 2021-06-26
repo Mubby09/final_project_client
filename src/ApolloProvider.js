cfe6dc6c7d1d8cdf37823cc2ba87b5b8b2f0db48
Mubarak Akinsola <akinsolamubarak1996@gmail.com>
2021-06-26 17:59:14 +0100

    edited proxy and server base URI

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000"
+  uri: "https://murmuring-oasis-83049.herokuapp.com"
 });
 
 const authLink = setContext(() => {
